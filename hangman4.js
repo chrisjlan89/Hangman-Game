63e422b2c86138ec9c2cc948627133366f61097f
Chris Lantier <chrisj17@gmail.com>
2017-11-18 10:22:25 -0500

    sat morning

--- hangman4.js
+++ hangman4.js
@@ -3,10 +3,15 @@ var guess = [];
 var guessesUsed =0;
 var wordLetters = [];
 var guessWord = [];
-var guesses = 7;
+var guesses = 8;
 var wins = 0;
 var losses = 0;
+var rightGuessCount = 0;
 var wrongLetters = [];
+
+
+
+
 
 
 
@@ -17,11 +22,18 @@ var wrongLetters = [];
    function randWord() {
 	 guessWord = words[Math.floor(Math.random() * words.length)];
 	 wordLetters = guessWord.split("");
-	 console.log("wordLetters.length" + wordLetters.length);
+	 console.log("Length of word string : " + wordLetters.length);
 	console.log(guessWord);
 	 console.log(wordLetters);
 	 for(var i=0; i<wordLetters.length; i++){
 	guess.push("_");
+	 
+     //Line 32 Problem
+	 //document.getElementById("rightGuessCount").innerHTML = guess.join("");
+
+
+
+
 
 	}
 
@@ -41,29 +53,71 @@ document.onkeypress = function compareWord(event) {
 	console.log(letterGuess);
      console.log(wordLetters.length);
 // }
+   if (wordLetters.indexOf(letterGuess) > -1) {
+
 	for(var i = 0; i < wordLetters.length; i++){
-		console.log("hello");
 		if (wordLetters[i] === letterGuess){
-			console.log("inside if statement");
 			guess[i] = letterGuess;
+			rightGuessCount++;
+			console.log("Your total right guesses: " + rightGuessCount);
             console.log("guess: " + guess);
-            document.getElementById("rightGuesses").innerHTML = guess.join("");
+             document.getElementById("rightGuesses").innerHTML = guess.join("");
 
             
 			
-               
+           
 		}
-       // running through the whole string taking away a guess on every letter. 
-			//even correct ones if they are not correct in every spot...which they wouldnt be
+	}
+}
+       
 		else {                              
-			wrongLetters.push(event.key);
+			
+			wrongLetters.push(letterGuess);
 			guesses --;
-			console.log("these letters are wrong: " + wrongLetters);
+		    console.log("these letters are wrong: " + wrongLetters);
 			console.log("remaining guesses: " + guesses);
+			document.getElementById("guessed").innerHTML = wrongLetters
+			document.getElementById("Remaining").innerHTML = guesses
 		}
 
 
 	}
 
-}
 
+
+	/*function reset(){
+      guess = [];
+      guessesUsed =0;
+      wordLetters = [];
+      guessWord = [];
+      guesses = 8;
+      wrongLetters = [];
+      randWord();
+	}
+*/
+
+	function winLose(){
+       if (guess == wordLetters) {
+         alert("You can be my huckleberry");
+         wins++;
+         console.log("your current win total")
+         reset();
+       }
+   }
+        
+       // else if (guesses === 0){
+       // alert("Maybe next time cowboy")
+       //losses++;
+       // reset();
+guess == wordLetters
+
+
+
+//function winLose(){
+  //     if (rightGuessCount == wordLetters.length) {
+    //     alert("You can be my huckleberry");
+    //     wins++;
+    //     console.log("your current win total")
+    //     reset();
+    //   }
+  // }
